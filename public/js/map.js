9183ea60c5554e8978d7ce1ca20c8cc862d33cd1
Ritesh gupta <Riteshguptarg419@gmail.com>
2024-09-25 15:41:47 +0530

    first commit

---
+++ public/js/map.js
@@ -0,0 +1,15 @@
+
+   mapboxgl.accessToken = mapToken ;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: "mapbox://styles/mapbox/streets-v12",
+        zoom: 8,
+        center: listing.geometry.coordinates
+    });
+
+    const marker = new mapboxgl.Marker({
+        color: "#fe424d",
+        
+    }).setLngLat(listing.geometry.coordinates)
+        .addTo(map);
+    
