881b85cb3db00a8e85038d6f36f99d3f94c603ec
Ritesh Gupta <163330297+Riteshgupta29@users.noreply.github.com>
2024-09-27 18:52:37 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing.js");
+const mbxGeoCoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN
+const geoCodingClient = mbxGeoCoding({ accessToken: mapToken });
+
 
 module.exports.index = async (req, res , next) => {
     const AllListings=  await Listing.find({});
@@ -16,16 +20,26 @@ module.exports.show =async (req,res) =>{
         req.flash("error"," Listing does not exist");
         res.redirect("/Listings") 
     };
+
     res.render("listings/show.ejs",{listing});
     };
 
-module.exports.creat = async (req, res, next)=> {
-    const newlisting = new Listing (req.body.vlisting);
-    newlisting.owner = req.user._id;
-    await newlisting.save();
-    req.flash("success","New Listing added succesfully");
-    res.redirect("/listings");
-    };
+    module.exports.creat = async (req, res, next)=> {
+     let response = await geoCodingClient.forwardGeocode({
+            query: req.body.vlisting.location,
+            limit:1
+            }).
+            send()
+        const url = req.file.path;
+        const filename = req.file.filename;
+        const newlisting = new Listing (req.body.vlisting);
+        newlisting.image={url,filename}
+        newlisting.owner = req.user._id;
+        newlisting.geometry = response.body.features[0].geometry;
+        await newlisting.save();
+        req.flash("success","New Listing added succesfully");
+         res.redirect("/listings");
+        };
 
     module.exports.edit =  async(req, res)=>{
         let {id}=req.params;
@@ -34,16 +48,24 @@ module.exports.creat = async (req, res, next)=> {
             req.flash("error"," Listing does not exist");
             res.redirect("/Listings") 
         };
+        // let originalUrl = listing.image.url;
+        // originalUrl.replace("/upload", "/upload/h_300,w_250");
         res.render("listings/edit.ejs", {listing});
         };
 
  module.exports.update =async (req, res) =>{
-    if (!req.body.vlisting){
-        throw new ExpressError(400,"clint error");}
-    let {id}=req.params; 
-    await Listing.findByIdAndUpdate (id, {...req.body.vlisting});
+    // if (!req.body.vlisting){
+    //     throw new ExpressError(400,"clint error");}
+    let {id}=req.params;
+    let listing = await Listing.findByIdAndUpdate (id, {...req.body.vlisting});
+    if(typeof req.file !=="undefined"){
+    const url = req.file.path;
+    const filename = req.file.filename;
+    listing.image = {url, filename};
+    await listing.save();
+ }  
     req.flash("success"," Listing updated succesfully");
-    res.redirect ("/Listings");
+    res.redirect ("/Listings")
     };      
 
     module.exports.delete =async (req, res) =>{
@@ -51,4 +73,4 @@ module.exports.creat = async (req, res, next)=> {
         await Listing.findByIdAndDelete(id);
         req.flash("success"," Listing deleted succesfully");
         res.redirect("/Listings");
-        };   
+        }
