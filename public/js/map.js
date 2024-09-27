03a3e62d2ba2b8226ba362da5859cb42c1f9f598
Ritesh Gupta <163330297+Riteshgupta29@users.noreply.github.com>
2024-09-27 20:26:37 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -5,11 +5,12 @@
         style: "mapbox://styles/mapbox/streets-v12",
         zoom: 8,
         center: listing.geometry.coordinates
-    });
-
-    const marker = new mapboxgl.Marker({
+      const marker = new mapboxgl.Marker({
         color: "#fe424d",
         
     }).setLngLat(listing.geometry.coordinates)
         .addTo(map);
+    
+    });
+
     
