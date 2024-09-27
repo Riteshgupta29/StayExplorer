e716dee302a7afce56d48e77f6649da2c5595353
Ritesh Gupta <163330297+Riteshgupta29@users.noreply.github.com>
2024-09-27 19:16:42 +0530

    Update Listing.js

--- routes/Listing.js
+++ routes/Listing.js
@@ -1,30 +1,33 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/WrapAsync");
-const {isLoggedIn, isOwner, validateListing} = require("../middleware.js")
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage });
 
-const controller = require("../controllers/listing.js")  
+const controller = require("../controllers/listing.js");
 
-
-//index route
-router.get("/", wrapAsync(controller.index )); 
+router
+  .route("/")
+  .get(wrapAsync(controller.index))
+  .post(
+    isLoggedIn,
+    upload.single("vlisting[image]"),
+    validateListing,
+    wrapAsync(controller.creat)
+  );
 
 //new route
-router.get("/new",isLoggedIn ,controller.new);
+router.get("/new", isLoggedIn, controller.new);
 
-//show route
-router.get("/:id",  wrapAsync( controller.show));
-
-//creat route
-router.post("/", validateListing ,isLoggedIn , wrapAsync (controller.creat)); 
+router
+  .route("/:id")
+  .get(wrapAsync(controller.show))
+  .put(isLoggedIn, isOwner, upload.single("vlisting[image]"), wrapAsync(controller.update))
+  .delete(isLoggedIn, isOwner, wrapAsync(controller.delete));
 
 //edit route
-router.get("/:id/edit", isLoggedIn ,isOwner,  wrapAsync(controller.edit));
-
-//update route
-router.put("/:id",isLoggedIn,isOwner, wrapAsync( controller.update));
-
-//delete route
-router.delete("/:id",isLoggedIn, isOwner,  wrapAsync( controller.delete));  
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(controller.edit));
 
 module.exports = router;
